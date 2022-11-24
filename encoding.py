24e79995ba13663c50098e11ab228ae1c80f4aab
Sun <525226851@qq.com>
2022-11-24 17:18:20 +0800

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -7,11 +7,11 @@ from retinaface import Retinaface
 '''
 retinaface = Retinaface(1)
 
-list_dir = os.listdir("face_dataset")
+list_dir = os.listdir("/content/facenet-retinaface-pytorch/face_dataset")
 image_paths = []
 names = []
 for name in list_dir:
-    image_paths.append("face_dataset/"+name)
+    image_paths.append("/content/facenet-retinaface-pytorch/face_dataset/"+name)
     names.append(name.split("_")[0])
 
 retinaface.encode_face_dataset(image_paths,names)
